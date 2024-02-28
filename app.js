111f9be5b2602bfc20b6d05ce6bd588699b384c1
Agustinabonnahon <158834103+Agustinabonnahon@users.noreply.github.com>
2024-02-27 22:26:06 -0300

    Add files via upload
    Subo el archivo app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express= require ("express");
+const app = express ();
+
+const path= require ("path");
+const publicPath=path.resolve(__dirname, "./public");
+app.use (express.static (puclicPath));
+
+const port = proess.env.PORT || 3000;
+app.listen (port, () =>
+console.log ('Servidor corriendo en Puerto $❴port❵')
+);
+
+app.get ("/", (req,res)=>❴
+    res.sendFile(path.resolve(__dirname, "./views/home.html"))
+    ❵);
+
+app.get ("/", (req,res)=>❴
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+    ❵);   
+
+app.post("/registro", (req,res)=>❴
+    res.send("Registro Exitoso!.")
+    ❵);
+
+app.get("/login", (req,res)=>❴
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+    ❵);    
